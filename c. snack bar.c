a97dc255915039e71b8ef21098222a1fbe10d08c
jackienjoroge <105638358+jackienjoroge@users.noreply.github.com>
2022-07-14 19:47:34 +0300

    Add files via upload

---
+++ c. snack bar.c
@@ -0,0 +1,51 @@
+Author: Njoroge jackline
+compiler: C89
+License :MIT
+Date: July 2022
+
+*/
+#include<"stdio.h">
+#include<"stdio.h">
+int main()
+{
+    printf("welcome to zetech c snack shop!\n");
+
+     menu("hamburger,hotdog,french fries,milkshake,coffee,beef smokies");
+    
+
+}
+    int menu()
+    { 
+    int item;
+    printf("1. 1 hotdog - ksh 90\n");
+    printf("2. 1 hamburger - ksh 300\n");
+    printf("3. 2 french fries - ksh 150\n");
+    printf("4. 2 milk shake - ksh 150\n");
+    printf("5. 2 coffee - ksh 70\n");
+    printf("6. 2 beef smokies - ksh 40\n"); 
+    printf("enter item number:");
+    scanf("%16",&item);
+    if(item>0"&:" item<=10);
+    printf("items added to cart\n");
+
+    }
+    // computation
+    hotdog ksh 90*1=ksh 90
+    hamburger 300*1= 300
+    french fries 150*2= 300
+    milkshake 150*2= 300
+    coffee 70*2= 140
+    beef smokies 40*2= 80
+
+    subtotal =  ksh 1210
+    VAT =           193.6
+    Total price = ksh 1403.6
+
+    Amount tendered = ksh 1550
+    change = ksh 146.4
+
+    THANK YOU AND COME AGAIN
+
+    }
+
+     return 0;
